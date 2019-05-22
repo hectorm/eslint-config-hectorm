4f724ba93d1b36ba71ad29148268966ad7df7e40
Héctor Molinero Fernández <hector@molinero.dev>
2019-05-22 20:54:39 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# eslint-config-hectorm
+
+> ESLint [shareable config](http://eslint.org/docs/developer-guide/shareable-configs.html).
---
+++ index.js
@@ -0,0 +1,28 @@
+module.exports = {
+	extends: ['airbnb-base'],
+	rules: {
+		'array-bracket-spacing': ['error', 'never'],
+		'arrow-body-style': ['off'],
+		'arrow-parens': ['error', 'as-needed'],
+		'camelcase': ['error', { properties: 'always' }],
+		'class-methods-use-this': ['off'],
+		'comma-dangle': ['error', 'never'],
+		'import/prefer-default-export': ['off'],
+		'indent': ['error', 'tab'],
+		'max-len': ['off'],
+		'no-bitwise': ['off'],
+		'no-console': ['warn', { allow: ['error', 'warn'] }],
+		'no-extra-semi': ['error'],
+		'no-param-reassign': ['off'],
+		'no-plusplus': ['off'],
+		'no-restricted-syntax': ['off'],
+		'no-tabs': ['error', { allowIndentationTabs: true }],
+		'no-underscore-dangle': ['off'],
+		'object-curly-newline': ['error', { multiline: true, consistent: true }],
+		'object-curly-spacing': ['error', 'always'],
+		'prefer-destructuring': ['off'],
+		'quotes': ['error', 'single'],
+		'semi': ['error', 'always'],
+		'wrap-iife': ['error', 'inside']
+	}
+};
